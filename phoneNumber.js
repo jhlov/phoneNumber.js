1f89898a76c636b0d0e1fed052383d85f3456867
jhlov <jhlovv@gmail.com>
2021-03-05 17:31:36 +0900

    Create phoneNumber.js

---
+++ phoneNumber.js
@@ -0,0 +1,37 @@
+function phoneNumber(value) {
+  if (!value) {
+    return "";
+  }
+
+  value = value.replace(/[^0-9]/g, "");
+
+  let result = [];
+  let restNumber = "";
+
+  // 지역번호와 나머지 번호로 나누기
+  if (value.startsWith("02")) {
+    // 서울 02 지역번호
+    result.push(value.substr(0, 2));
+    restNumber = value.substring(2);
+  } else if (value.startsWith("1")) {
+    // 지역 번호가 없는 경우
+    // 1xxx-yyyy
+    restNumber = value;
+  } else {
+    // 나머지 3자리 지역번호
+    // 0xx-yyyy-zzzz
+    result.push(value.substr(0, 3));
+    restNumber = value.substring(3);
+  }
+
+  if (restNumber.length === 7) {
+    // 7자리만 남았을 때는 xxx-yyyy
+    result.push(restNumber.substring(0, 3));
+    result.push(restNumber.substring(3));
+  } else {
+    result.push(restNumber.substring(0, 4));
+    result.push(restNumber.substring(4));
+  }
+
+  return result.filter((val) => val).join("-");
+}
